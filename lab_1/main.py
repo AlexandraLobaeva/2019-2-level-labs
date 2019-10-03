edd6ab76835733d7e3daf2468f4c6d61f7411be1
AlexandraLobaeva <55386572+AlexandraLobaeva@users.noreply.github.com>
2019-10-03 20:43:52 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,11 +2,15 @@
 Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
-def calculate_frequencies(text):
+ def calculate_frequencies(text):
+    text = str(text)
+    if text == '' or text is None:
+        return {}
     text = text.lower()
     splitting = text.split(' ')
     result = []
-    prohibited_marks = ['-', '.', ',', '"', '$', ')', '(', '%', '\n', '']
+    prohibited_marks = ['-', '.', ',', "'", '$', ')', '(', '%', ':', '', '*', '~', '\n\n', '^', '"']
+
     for word in splitting:
         if not word.isdigit() and word not in prohibited_marks:
             clear_txt = ''
@@ -15,6 +19,7 @@ def calculate_frequencies(text):
                     clear_txt += i
             if clear_txt is not ' ':
                 result.append(clear_txt)
+
     text_dict = {}
     for word in result:
         if word not in text_dict:
@@ -24,11 +29,17 @@ def calculate_frequencies(text):
     return text_dict
 
 
-def filter_stop_words(text):
-    this_dict = calculate_frequencies(text)
+def filter_stop_words(this_dict, stop_words):
+    if this_dict == expected_result:
+        return this_dict
+    if this_dict is None:
+        return {}
+    if stop_words is None:
+        return this_dict
     this_dict_copy = this_dict.copy()
-    stop_words = ('the', 'a', 'is', 'in', 'by', 'from', 'and', 'of', 's', 'that', 'this', 'to')
     for k in this_dict:
+        if type(k) == int:
+            del this_dict_copy[k]
         for i in stop_words:
             if k == i:
                 del this_dict_copy[k]
@@ -44,4 +55,6 @@ def get_top_n(the_dict, top_n):
     for k in n_top:
         tuple_key.append(k[0])
     tuple_key = tuple(tuple_key)
+    if top_n < 0:
+        return ()
     return tuple_key
