af73d3c5851014912b10103e2a5022c877678bc8
AlexandraLobaeva <55386572+AlexandraLobaeva@users.noreply.github.com>
2019-12-17 06:42:27 +0300

    Update main.py
    lab 4 added

--- lab_4/main.py
+++ lab_4/main.py
@@ -2,27 +2,94 @@ import math
 
 
 REFERENCE_TEXTS = []
+if __name__ == '__main__':
+    TEXTS = ['5_7.txt', '15_2.txt', '10547_3.txt', '12230_7.txt']
+    for text in TEXTS:
+        with open(text, 'r') as f:
+            REFERENCE_TEXTS.append(f.read())
 
-
-def clean_tokenize_corpus(texts: list) -> list:
-    pass
+def clean_tokenize_corpus(texts_corpus: list) -> list:
+    if not isinstance(texts_corpus, list) or not texts_corpus:
+        return []
+    clean_corpus = []
+    for text in texts_corpus:
+        if isinstance(text, str):
+            text = text.lower()
+            clean_text = []
+            br = '<br />'
+            while br in text:
+                text = text.replace(br, ' ')
+            one_text = text.split(' ')
+            for word in one_text:
+                new_word = ''
+                for i in word:
+                    if i.isalpha():
+                        new_word += i
+                if new_word != '':
+                    clean_text.append(new_word)
+            clean_corpus += [clean_text]
+    return clean_corpus
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
+        self.file_names = ['5_7.txt', '15_2.txt', '10547_3.txt', '12230_7.txt']
 
     def calculate_tf(self):
-        pass
+        if self.corpus is not None:
+            for text in self.corpus:
+                if text is not None and isinstance(text, list):
+                    tf_values = {}
+                    doc_len = len(text)
+                    for word in text:
+                        count_prob = 1
+                        if not isinstance(word, str):
+                            if word not in tf_values:
+                                tf_values[word] = count_prob / doc_len
+                            else:
+                                tf_values[word] += count_prob / doc_len
+                    self.tf_values.append(tf_values)
+        return self.tf_values
 
     def calculate_idf(self):
-        pass
+        if self.corpus is not None:
+            for text in self.corpus:
+                if text is not None:
+                    unique_words = []
+                    for word in text:
+                        if word is not None and word not in unique_words and isinstance(word, str):
+                            unique_words += [word]
+                    count_probs = {}
+                    for word in unique_words:
+                        count_prob = 0
+                        for text in self.corpus:
+                            if not text or word in text:
+                                count_prob += 1
+                        count_probs[word] = count_prob
+                        if count_probs.get(word) != 0:
+                            corpus_len = len(self.corpus)
+                            self.idf_values[word] = math.log(corpus_len / count_probs.get(word))
+        return self.idf_values
 
     def calculate(self):
-        pass
+        if self.idf_values and self.tf_values:
+            for text in self.tf_values:
+                tf_idf_values = {}
+                for word, tf_value in text.items():
+                    tf_idf_values[word] = tf_value * self.idf_values.get(word)
+                self.tf_idf_values.append(tf_idf_values)
+        return self.tf_idf_values
 
     def report_on(self, word, document_index):
-        pass
+        if not self.tf_idf_values or document_index >= len(self.tf_idf_values):
+            return ()
+        tf_idf_dict = self.tf_idf_values[document_index]
+        tf_idf_sort_list = sorted(tf_idf_dict, key=tf_idf_dict.__getitem__, reverse=True)
+        return tf_idf_dict.get(word), tf_idf_sort_list.index(word)
 
 
 if __name__ == '__main__':
